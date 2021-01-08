e37108fcdc1d6c22572c144b368a3eed6a46e794
FavourOyewumi7 <oyewumifavour65@gmail.com>
2021-01-08 06:45:02 +0100

    -todo proj using state management

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    
+    <div class="container"> 
+      <h1>Todos App</h1>
+      <AddTodo />
+      <FilterTodo />
+      <Todos /> 
+    </div>
+
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import FilterTodo from './components/FilterTodo'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Todos,
+    AddTodo,
+    FilterTodo
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  line-height: 1.6;
+  background: #e8f7f0;
+
+}
+.container{
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
 }
 </style>
---
+++ src/store/index.js
@@ -0,0 +1,97 @@
+
+
+import {createStore} from 'vuex';
+
+//Create Store
+const stores = createStore({
+    
+state :{
+
+    todos:[]
+},
+
+getters :{
+    allTodos: (state)=>{
+        return state.todos
+    }
+},
+
+actions :{
+    async fetchTodos({commit}){
+        const response =  await fetch('https://jsonplaceholder.typicode.com/todos');
+        const todoss = await response.json()
+        commit('setTodos', todoss)
+
+    },
+    async addTodo({commit}, title){
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos', {
+            method: 'POST',
+            mode:'cors',
+            body: JSON.stringify({
+              title: title,
+              completed: false
+            }),
+            headers: {
+              'Content-type': 'application/json; charset=UTF-8',
+            },
+          });
+        const result = await response.json()
+        
+
+
+        
+          commit('newTodo', result)
+
+
+    },
+    async deleteTodo({commit}, id){
+        console.log(id)
+        await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+            method: 'DELETE',
+          })
+          commit('removeTodo', id);
+
+    },
+    async filterTodo({commit},event){
+        const limit = parseInt(event.target.options[event.target.options.selectedIndex].innerText);
+
+        const response =  await fetch(`https://jsonplaceholder.typicode.com/todos?_limit=${limit}`);
+        const todoss = await response.json()
+        commit('setTodos', todoss)
+    },
+
+    async updateTodo({commit}, updTodo){
+        
+        const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${updTodo.id}`,{
+            method: 'PUT',
+            body: JSON.stringify({
+              updTodo
+            }),
+            headers: {
+              'Content-type': 'application/json; charset=UTF-8',
+            },
+          });
+        const todoss = await response.json()
+        
+        commit('updateTodo', todoss);
+
+    }
+},
+
+mutations :{
+    setTodos:(state, todos) => {state.todos = todos},
+    newTodo:(state, todo) => {state.todos.unshift(todo)},
+    removeTodo:(state, id) => {
+        const num = parseInt(id)
+        state.todos.splice(num-1,1)
+    },
+    updateTodo:(state, updTodo) =>{
+        const index = state.todos.findIndex(todo => todo.id === updTodo);
+        if (index !== -1){
+            state.todos.splice(index,1, updTodo);
+        }
+    }
+}
+})
+
+export default stores
